506f28050a909b2b7bf3b17192c35fd494c8193e
Grant Heffernan <heffergm@gmail.com>
2013-07-23 11:33:53 -0400

    test

--- site-cookbooks/monitor/attributes/default.rb
+++ site-cookbooks/monitor/attributes/default.rb
@@ -1,15 +1,20 @@
-default['sensu']['version'] = "0.10.0-2"
-default["monitor"]["master_address"] = nil
+case chef_environment
+when 'production'
+  default['sensu']['version'] = "0.10.0-2"
 
+  default["monitor"]["default_handlers"] = [ "debug", "pagerduty", "irc" ]
+  default["monitor"]["metric_handlers"]  = [ "debug" ]
+when 'staging'
+  default['sensu']['version'] = "0.10.0-2"
+
+  default["monitor"]["default_handlers"] = [ "debug", "irc" ]
+  default["monitor"]["metric_handlers"]  = [ "debug" ]
+end
+
+default["monitor"]["master_address"] = nil
 default["monitor"]["environment_aware_search"] = false
 default["monitor"]["use_local_ipv4"] = false
-
 default["monitor"]["sensu_plugin_version"] = "0.1.6"
-
 default["monitor"]["additional_client_attributes"] = Mash.new
-
 default["monitor"]["sudo_commands"] = Array.new
-
-default["monitor"]["default_handlers"] = [ "debug", "pagerduty", "irc" ]
-default["monitor"]["metric_handlers"]  = [ "debug" ]
 
--- site-cookbooks/monitor/attributes/handlers.rb
+++ site-cookbooks/monitor/attributes/handlers.rb
@@ -1,6 +1,15 @@
-default["monitor"]["pagerduty_api_key"] = "779b591489b9485a96ce7ae73ec1ac7c"
-default["monitor"]["irc_server"] = "irc://sensutest@irc.freenode.net#sensu123"
+case chef_environment
+when 'production'
+  default["monitor"]["pagerduty_api_key"] = "779b591489b9485a96ce7ae73ec1ac7c"
+  default["monitor"]["irc_server"] = "irc://sensutest@irc.freenode.net#sensu123"
 
-default["monitor"]["graphite_address"] = nil
-default["monitor"]["graphite_port"] = nil
+  default["monitor"]["graphite_address"] = nil
+  default["monitor"]["graphite_port"] = nil
+when 'staging'
+  default["monitor"]["pagerduty_api_key"] = ""
+  default["monitor"]["irc_server"] = "irc://sensutest@irc.freenode.net#sensu123_staging"
+
+  default["monitor"]["graphite_address"] = nil
+  default["monitor"]["graphite_port"] = nil
+end
 
