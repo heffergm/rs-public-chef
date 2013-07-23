59bd8c8087f40241917d99e7b36110d6d7aa6937
Grant Heffernan <heffergm@gmail.com>
2013-07-23 09:49:32 -0400

    1

--- environments/production.rb
+++ environments/production.rb
@@ -1,4 +1,5 @@
 name "production"
 description "The production environment"
 #cookbook_versions  "couchdb" => "= 11.0.0"
-#default_attributes "apache2" => { "listen_ports" => [ "80", "443" ] }
+default_attributes "monitor" => { "pagerduty_api_key" => "779b591489b9485a96ce7ae73ec1ac7c" }
+default_attributes "monitor" => { "irc_server" => "irc://sensutest@irc.freenode.net#sensu123" }
