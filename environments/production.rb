405c35a13da14be944f2e3be9bfe5319083dffee
Grant Heffernan <heffergm@gmail.com>
2013-07-23 11:23:48 -0400

    fix

--- environments/production.rb
+++ environments/production.rb
@@ -1,8 +1,3 @@
 name "production"
 description "The production environment"
-#cookbook_versions  "couchdb" => "= 11.0.0"
-default_attributes "monitor" => { "pagerduty_api_key" => "779b591489b9485a96ce7ae73ec1ac7c" }
-default_attributes "monitor" => { "irc_server"        => "irc://sensutest@irc.freenode.net#sensu123" }
-default_attributes "monitor" => { "default_handlers"  => [ "debug", "pagerduty", "irc" ] }
-default_attributes "monitor" => { "metric_handlers"   => [ "debug" ] }
 
--- site-cookbooks/monitor/attributes/handlers.rb
+++ site-cookbooks/monitor/attributes/handlers.rb
@@ -1,5 +1,6 @@
-default["monitor"]["pagerduty_api_key"] = "#{node[:monitor][:pagerduty_api_key]}"
-default["monitor"]["irc_server"] = "#{node[:monitor][:irc_server]}"
+default["monitor"]["pagerduty_api_key"] = "779b591489b9485a96ce7ae73ec1ac7c"
+default["monitor"]["irc_server"] = "irc://sensutest@irc.freenode.net#sensu123"
 
 default["monitor"]["graphite_address"] = nil
 default["monitor"]["graphite_port"] = nil
+
