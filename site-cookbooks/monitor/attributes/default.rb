fc4dc3a7341ef75c27c228c5b9064eecb897a2d2
Grant Heffernan <heffergm@gmail.com>
2013-07-23 11:43:33 -0400

    def

--- site-cookbooks/monitor/attributes/default.rb
+++ site-cookbooks/monitor/attributes/default.rb
@@ -11,10 +11,12 @@ when 'staging'
   default["monitor"]["metric_handlers"]  = [ "debug" ]
 end
 
-default["monitor"]["master_address"] = nil
-default["monitor"]["environment_aware_search"] = false
-default["monitor"]["use_local_ipv4"] = false
-default["monitor"]["sensu_plugin_version"] = "0.1.6"
+override["sensu"]["use_embedded_ruby"]             = true
+default["monitor"]["sensu_plugin_version"]         = "0.1.6"
+default["monitor"]["master_address"]               = nil
+default["monitor"]["environment_aware_search"]     = false
+default["monitor"]["use_local_ipv4"]               = false
+default["monitor"]["sensu_plugin_version"]         = "0.1.6"
 default["monitor"]["additional_client_attributes"] = Mash.new
-default["monitor"]["sudo_commands"] = Array.new
+default["monitor"]["sudo_commands"]                = Array.new
 
