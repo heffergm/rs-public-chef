5324ec53bb0ce713be94a6d13bb4c3726c7459e1
Grant Heffernan <heffergm@gmail.com>
2013-07-23 10:56:52 -0400

    test

--- site-cookbooks/monitor/attributes/handlers.rb
+++ site-cookbooks/monitor/attributes/handlers.rb
@@ -1,4 +1,4 @@
-default["monitor"]["pagerduty_api_key"] = ""
+default["monitor"]["pagerduty_api_key"] = "#{node[:foo][:bar]}"
 
 default["monitor"]["graphite_address"] = nil
 default["monitor"]["graphite_port"] = nil
