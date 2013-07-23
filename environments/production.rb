b9a0357e1cd9fd9416d4d9c7c6682375e907b971
Grant Heffernan <heffergm@gmail.com>
2013-07-23 10:34:04 -0400

    override

--- environments/production.rb
+++ environments/production.rb
@@ -1,8 +1,8 @@
 name "production"
 description "The production environment"
 #cookbook_versions  "couchdb" => "= 11.0.0"
-default_attributes "monitor" => { "pagerduty_api_key" => "779b591489b9485a96ce7ae73ec1ac7c" }
-default_attributes "monitor" => { "irc_server"        => "irc://sensutest@irc.freenode.net#sensu123" }
-default_attributes "monitor" => { "default_handlers"  => [ "debug", "pagerduty", "irc" ] }
-default_attributes "monitor" => { "metric_handlers"   => [ "debug" ] }
+override_attributes "monitor" => { "pagerduty_api_key" => "779b591489b9485a96ce7ae73ec1ac7c" }
+override_attributes "monitor" => { "irc_server"        => "irc://sensutest@irc.freenode.net#sensu123" }
+override_attributes "monitor" => { "default_handlers"  => [ "debug", "pagerduty", "irc" ] }
+override_attributes "monitor" => { "metric_handlers"   => [ "debug" ] }
 
